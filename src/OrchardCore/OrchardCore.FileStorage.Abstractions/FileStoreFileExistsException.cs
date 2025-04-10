844dff28b72aef963783d007b252f11a9cf440a1
DoX <DarkParadoX00@gmail.com>
2025-04-10 12:10:34 +0300

    Changed the logging type when trying to save a duplicate file

---
+++ src/OrchardCore/OrchardCore.FileStorage.Abstractions/FileStoreFileExistsException.cs
@@ -0,0 +1,12 @@
+﻿namespace OrchardCore.FileStorage;
+
+public class FileStoreFileExistsException : Exception
+{
+    public FileStoreFileExistsException(string message) : base(message)
+    {
+    }
+
+    public FileStoreFileExistsException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
